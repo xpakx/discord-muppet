73f86a5c4781fb411eff00e4d999e56aa56ced24
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-11 19:08:36 +0200

    refactor(backend): read options dynamically from spring environment

--- playwright-backend/src/main/java/io/github/xpakx/discord_muppet/page/PlaywrightConfig.java
+++ playwright-backend/src/main/java/io/github/xpakx/discord_muppet/page/PlaywrightConfig.java
@@ -1,9 +1,9 @@
 package io.github.xpakx.discord_muppet.page;
 
 import com.microsoft.playwright.Playwright;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,11 +11,14 @@ import java.util.Map;
 @Configuration
 public class PlaywrightConfig {
 
-    // TODO: get all options
     @Bean
-    public Playwright.CreateOptions createOptions(@Value("${playwright.skip.browser.download}") String skipDownload) {
+    public Playwright.CreateOptions createOptions(Environment environment) {
         Map<String, String> optionsMap = new HashMap<>();
-        optionsMap.put("PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD", skipDownload);
+        String skipDownload = environment.getProperty("playwright.skip.browser.download");
+        if(skipDownload != null) {
+            optionsMap.put("PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD", skipDownload);
+        }
+        // TODO: get more options
         var options = new Playwright.CreateOptions();
         options.setEnv(optionsMap);
         return options;
