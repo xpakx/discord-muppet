d78b00ec8262d9696da9c063dc1a6419adf1f287
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-11 15:27:32 +0200

    feat(backend): open and fill login form

--- playwright-backend/src/main/java/io/github/xpakx/discord_muppet/DiscordMuppetApplication.java
+++ playwright-backend/src/main/java/io/github/xpakx/discord_muppet/DiscordMuppetApplication.java
@@ -1,11 +1,22 @@
 package io.github.xpakx.discord_muppet;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class DiscordMuppetApplication implements CommandLineRunner {
+	@Value("${discord.credentials.email}")
+	String email;
+
+	@Value("${discord.credentials.password}")
+	String password;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscordMuppetApplication.class, args);
@@ -13,6 +24,18 @@ public class DiscordMuppetApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) {
-		System.out.println("Command line runner");
+		try (Playwright playwright = Playwright.create();
+		 Browser browser = playwright.chromium().launch()) {
+			Page page = browser.newPage();
+			page.navigate("https://discord.com/app");
+			System.out.println(page.title());
+			page.locator("input[name='email']")
+					.fill(email);
+			page.locator("input[name='password']")
+					.fill(password);
+			page.screenshot(
+					new Page.ScreenshotOptions().setPath(Paths.get("debug/screenshot.png"))
+			);
+		}
 	}
 }
