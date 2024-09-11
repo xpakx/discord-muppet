293f97be5d73aa9a2dda72933be1ac0d7c528952
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-11 15:58:10 +0200

    chore(backend): add logging to playwright wrapper

--- playwright-backend/src/main/java/io/github/xpakx/discord_muppet/page/PageWrapper.java
+++ playwright-backend/src/main/java/io/github/xpakx/discord_muppet/page/PageWrapper.java
@@ -4,6 +4,8 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.nio.file.Path;
@@ -14,22 +16,32 @@ public class PageWrapper {
     private final Page page;
     private final Browser browser;
     private final Playwright playwright;
+    Logger logger = LoggerFactory.getLogger(PageWrapper.class);
 
     public PageWrapper() {
         try  {
             this.playwright = Playwright.create();
+            logger.info("Starting browser");
             this.browser = playwright.chromium().launch();
+            logger.info("Opening new tab");
             this.page = browser.newPage();
         } catch (Exception e) {
+            logger.error("Couldn't start browser!");
             throw new RuntimeException(e);
         }
     }
 
     public void goToLogin() {
+        logger.info("Navigating to login page");
         page.navigate("https://discord.com/app");
     }
 
     public void fillLoginForm(String email, String password) {
+        logger.info(
+                "Filling login form with email: {} and password {}",
+                email,
+                "*".repeat(password.length())
+        );
         page.locator("input[name='email']")
                 .fill(email);
         page.locator("input[name='password']")
@@ -50,7 +62,7 @@ public class PageWrapper {
 
     @PreDestroy
     public void preDestroy() {
-        System.out.println("Closing browser…");
+        logger.info("Closing browser…");
         browser.close();
         playwright.close();
     }
