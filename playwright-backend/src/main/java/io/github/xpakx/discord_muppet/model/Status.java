af0c9138a313515b35f0a3e2f884b1ff9077df0e
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-16 14:15:50 +0200

    chore(backend): add more statuses

--- playwright-backend/src/main/java/io/github/xpakx/discord_muppet/model/Status.java
+++ playwright-backend/src/main/java/io/github/xpakx/discord_muppet/model/Status.java
@@ -1,16 +1,15 @@
 package io.github.xpakx.discord_muppet.model;
 
-import java.util.Optional;
-
 public enum Status {
-    Online, Invisible, Idle, DoNotDisturb, Unknown;
+    Online, Invisible, Idle, DoNotDisturb, Offline, Unknown;
 
     public static Status toStatus(String string) {
         return switch (string) {
-            case "Online" -> Online;
-            case "Invisible" -> Invisible;
+            case "Online", "Dostępny" -> Online;
+            case "Invisible", "Niewidoczny" -> Invisible;
             case "Idle" -> Idle;
             case "Do Not Disturb" -> DoNotDisturb;
+            case "Offline", "Niedostępny" -> Offline;
             default -> Unknown;
         };
     }
