a400ebaada8c416f7354350bfd74baf1adeb3b8c
xpakx <55586470+xpakx@users.noreply.github.com>
2024-09-20 14:40:46 +0200

    chore(backend): configure websocket

---
+++ playwright-backend/src/main/java/io/github/xpakx/discord_muppet/websocket/WebsocketService.java
@@ -0,0 +1,29 @@
+package io.github.xpakx.discord_muppet.websocket;
+
+import io.github.xpakx.discord_muppet.conversation.MessageItem;
+import io.github.xpakx.discord_muppet.model.dto.FriendData;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WebsocketService {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+
+    public WebsocketService(SimpMessagingTemplate simpMessagingTemplate) {
+        this.simpMessagingTemplate = simpMessagingTemplate;
+    }
+
+    public void updateFriendList(List<FriendData> msg) {
+        simpMessagingTemplate.convertAndSend("/topic/friends", msg);
+    }
+
+    public void updateConversation(List<MessageItem> msg) {
+        simpMessagingTemplate.convertAndSend("/topic/current", msg);
+    }
+
+    public void openConversation(List<MessageItem> msg) {
+        simpMessagingTemplate.convertAndSend("/topic/open", msg);
+    }
+}
